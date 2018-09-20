2aaea199f8621e8d936f93e2ca74a5367c3a3d74
Paolo Cozzi <bunop@libero.it>
2018-09-20 12:04:20 +0200

    :sparkles: First unit test and url mapping, dummy view

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 11:55:08 2018
+
+@author: Paolo Cozzi <cozzi@ibba.cnr.it>
+"""
+
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_page():
+    pass
